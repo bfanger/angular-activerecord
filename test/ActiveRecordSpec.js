d3c5cc2a844d0a79dd5c53e1c874b1b6dfbfa8b4
Bob Fanger <bfanger@gmail.com>
2013-06-08 21:47:51 +0200

    Added UnitTest (from Backbone) for additional compatibility
    https://github.com/documentcloud/backbone/blob/master/test/model.js

--- test/ActiveRecordSpec.js
+++ test/ActiveRecordSpec.js
@@ -1,4 +1,5 @@
 describe("ActiveRecord", function() {
+	"use strict";
 
 	beforeEach(angular.mock.module("ActiveRecord"));
 
@@ -14,15 +15,57 @@ describe("ActiveRecord", function() {
 		$httpBackend.verifyNoOutstandingRequest();
 	});
 
-	it('fetches data', function () {
-		var TestRecord = ActiveRecord.extend({
+	it("initialize", function() {
+		var Model = ActiveRecord.extend({
+			$initialize: function() {
+				this.one = 1;
+			}
+		});
+		var model = new Model();
+		expect(model.one).toBe(1);
+	});
+
+	it("initialize with attributes and options", function() {
+		var Model = ActiveRecord.extend({
+			$initialize: function(attributes, options) {
+				this.one = options.one;
+			}
+		});
+		var model = new Model({}, {one: 1});
+		expect(model.one).toBe(1);
+	});
+
+	it("initialize with parsed attributes", function() {
+		var Model = ActiveRecord.extend({
+			$parse: function(attrs) {
+				attrs.value += 1;
+				return attrs;
+			}
+		});
+		var model = new Model({value: 1}, {parse: true});
+		expect(model.value).toBe(2);
+	});
+
+	it("initialize with defaults", function() {
+		var Model = ActiveRecord.extend({
+			defaults: {
+				first_name: 'Unknown',
+				last_name: 'Unknown'
+			}
+		});
+		var model = new Model({first_name: 'John'});
+		expect(model.first_name, 'John');
+		expect(model.last_name, 'Unknown');
+	});
+
+	it('fetchOne', function () {
+		var Model = ActiveRecord.extend({
 	 		$urlRoot: '/resources/'
 		});
-
-		$httpBackend.expectGET('/resources/1').respond( {id: '1', name: 'Test1'});
-		TestRecord.fetchOne(1).then(function (test1) {
-			expect(test1.id).toBe('1');
-			expect(test1.name).toBe('Test1');
+		$httpBackend.expectGET('/resources/1').respond( {id: 1, name: 'Test1'});
+		Model.fetchOne(1).then(function (model) {
+			expect(model.id).toBe(1);
+			expect(model.name).toBe('Test1');
 		});
 		$httpBackend.flush();
 	 });
