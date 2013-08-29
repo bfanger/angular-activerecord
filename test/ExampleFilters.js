1a7b24f84aabd5d40ae382156eac446ac339a599
tiraeth <tiraeth@gmail.com>
2013-08-29 12:35:14 +0200

    fixed indentation

--- test/ExampleFilters.js
+++ test/ExampleFilters.js
@@ -5,35 +5,35 @@ angular.module('ExampleFilters', ['ng'])
 	 */
 	.filter('toDateObject', function() {
 		var fromIsoString = (function(){
-            var tzoffset = (new Date).getTimezoneOffset();
-            function fastDateParse(y, m, d, h, i, s, ms){ // this -> tz
-                return new Date(y, m - 1, d, h || 0, +(i || 0) - this, s || 0, ms || 0);
-            }
+			var tzoffset = (new Date).getTimezoneOffset();
+			function fastDateParse(y, m, d, h, i, s, ms){ // this -> tz
+				return new Date(y, m - 1, d, h || 0, +(i || 0) - this, s || 0, ms || 0);
+			}
 
-            // result function
-            return function(isoDateString){
-                var tz = isoDateString.substr(10).match(/([\-\+])(\d{1,2}):?(\d{1,2})?/) || 0;
-                if (tz) {
-                    tz = tzoffset + (tz[1] == '-' ? -1 : 1) * (tz[3] != null ? +tz[2] * 60 + (+tz[3]) : +tz[2]);
-                } else {
-                    tz = tzoffset;
-                }
-                return fastDateParse.apply(tz || 0, isoDateString.split(/\D/));
-            }
-        })();
+			// result function
+			return function(isoDateString){
+				var tz = isoDateString.substr(10).match(/([\-\+])(\d{1,2}):?(\d{1,2})?/) || 0;
+				if (tz) {
+					tz = tzoffset + (tz[1] == '-' ? -1 : 1) * (tz[3] != null ? +tz[2] * 60 + (+tz[3]) : +tz[2]);
+				} else {
+					tz = tzoffset;
+				}
+				return fastDateParse.apply(tz || 0, isoDateString.split(/\D/));
+			}
+		})();
 
 		return function(value) {
 			if (typeof value === 'string') {
-                var match = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)(?:([\+-])(\d{2})\:(\d{2}))?Z?$/.exec(value);
-                
-                return match ? fromIsoString(value) : value;
-	        }
+				var match = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)(?:([\+-])(\d{2})\:(\d{2}))?Z?$/.exec(value);
+				
+				return match ? fromIsoString(value) : value;
+			}
 
-	        return value;
+			return value;
 		};
 	}).filter('toISO8601', function(value) {
-		 if (value instanceof Date) {
-			 return value.toISOString();
-		 }
-		 return value;
+		if (value instanceof Date) {
+			return value.toISOString();
+		}
+		return value;
 	});
